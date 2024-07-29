ec5eb57735c3a402c69adb004932e22a6aeefc8a
Yauheni Kuntsevich <jauhien.k@gmail.com>
2024-07-29 17:20:12 +0300

    gitignore update

--- main.py
+++ main.py
@@ -21,53 +21,52 @@ dp = Dispatcher()
 
 
 bot_commands = [
-    types.BotCommand(command='/help', description='Як я працую?'),
+    types.BotCommand(command="/help", description="Як я працую?"),
 ]
 
 
 @dp.message(CommandStart())
 async def handle_start(message: types.Message):
     await message.answer(
-        text=f'Вітанкі, {message.from_user.full_name}!\n\n{START_MESSAGE}',
-        parse_mode='HTML'
+        text=f"Вітанкі, {message.from_user.full_name}!\n\n{START_MESSAGE}",
+        parse_mode="HTML",
     )
 
 
-@dp.message(Command('topic'))
+@dp.message(Command("topic"))
 async def handle_topic(message: types.Message):
     await message.answer(
-        text='На якую тэму табе хочацца атрымаць цытату?',
-        reply_markup=topics_keyboard
+        text="На якую тэму табе хочацца атрымаць цытату?", reply_markup=topics_keyboard
     )
 
 
-@dp.message(Command('works'))
+@dp.message(Command("works"))
 async def handle_works(message: types.Message):
     pass
 
 
-@dp.message(Command('help'))
+@dp.message(Command("help"))
 async def handle_help(message: types.Message):
     pass
 
 
 @dp.message()
-async def handle_topic_choice(message: types):
+async def handle_topic_choice(message: types.Message):
     if message.text in topics:
         random_quote = get_random_quote_by_topic(message.text)
         await message.answer(
-            text=f'{random_quote.get('text')}\n\n<b>{random_quote.get('author')}</b>',
-            parse_mode='HTML'
+            text=f"{random_quote.get('text')}\n\n<b>{random_quote.get('author')}</b>",
+            parse_mode="HTML",
         )
 
 
 async def main():
     logging.basicConfig(level=logging.DEBUG)
-    
+
     await bot.set_my_commands(BOT_COMMANDS)
 
     await dp.start_polling(bot)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(main())
