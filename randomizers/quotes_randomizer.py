52f7117ada5d69bbc534751d43f9583208db0271
Yauheni Kuntsevich <jauhien.k@gmail.com>
2024-08-02 12:03:21 +0300

    get_random_quote_by_author() was created

--- randomizers/quotes_randomizer.py
+++ randomizers/quotes_randomizer.py
@@ -1,9 +1,21 @@
 from random import randint
 from data.quotes import QUOTES
+from data.quotes import AUTHORS
 
 
 def get_random_quote_by_topic(topic):
     quotes_by_topic = QUOTES.get(topic)
 
-    random_index = randint(1, len(quotes_by_topic) - 1)
+    random_index = randint(0, len(quotes_by_topic) - 1)
     return quotes_by_topic[random_index]
+
+
+def get_random_quote_by_author(author):
+    quotes_by_author = []
+
+    for quote in QUOTES:
+        if quote.get("author") == author:
+            quotes_by_author.append(quote)
+
+    random_index = randint(0, len(quotes_by_author) - 1)
+    return quotes_by_author[random_index]
